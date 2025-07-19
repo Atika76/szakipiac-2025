22d76d7500072482a8412c6038d0abc2811558c6
Atika76 <139039295+Atika76@users.noreply.github.com>
2025-07-19 12:29:32 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,3 @@
-// supabase.js tartalma (ha külön van, ezt NE írd bele mégegyszer!)
-// Ha minden JS egy fájlban van, akkor maradhat itt is:
 const SUPABASE_URL = "https://bxtpnotswnwrbycvfypz.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4dHBub3Rzd253cmJ5Y3ZmeXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MTQ0NDcsImV4cCI6MjA2ODQ5MDQ0N30.CXEfo_8qmIYhkEZFdTsbl9ZB-PRTP6UK8EbIxxpSGZc";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
