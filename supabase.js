0ced383c5a22089bdd89d04ba3c4df8bec1110c9
Atika76 <139039295+Atika76@users.noreply.github.com>
2025-08-16 15:50:03 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,5 +0,0 @@
-// supabase.js
-const SUPABASE_URL = "https://bxtpnotswnwrbycvfypz.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4dHBub3Rzd253cmJ5Y3ZmeXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MTQ0NDcsImV4cCI6MjA2ODQ5MDQ0N30.CXEfo_8qmIYhkEZFdTsbl9ZB-PRTP6UK8EbIxxpSGZc";
-
-const supaClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
